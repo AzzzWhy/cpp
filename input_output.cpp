85d438afd6b4732653fb8573bb7bd9b08ee7ad44
azzwhy <2035698109@qq.com>
2026-03-17 13:26:06 +0800

    I_O

---
+++ input_output.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //整型
+    int a;
+    cout<<"input something init"<<endl;
+    cin>>a;
+    cout<<"output "<<a<<endl;
+    //浮点
+    float b;
+    cin>>b;
+    cout<<"b"<<b<<endl;
+    //字符型
+    char c='a';
+    cout<<"c"<<endl;
+    cin>>c;
+    cout<<c<<endl;
+    //字符串型
+    string d;
+    cout<<d<<endl;
+    cin>>d;
+    cout<<"d"<<d<<endl;
+    //bool
+    bool flag=false;
+    cout<<flag<<endl;
+    cin>>flag;
+    cout<<"bool"<<flag<<endl;
+
+
+
+
+
+
+
+
+
+    return 0;
+}
