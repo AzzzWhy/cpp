a10ebcdb6997c687dec895db0d3f92d584fabd21
azzwhy <2035698109@qq.com>
2026-03-17 20:54:53 +0800

    V4.1_END

--- First_step/if_else.cpp
+++ First_step/if_else.cpp
@@ -1,50 +1,46 @@
 #include <iostream>
 using namespace std;
+//函数部分
 
-// 找最大
+// 找最大函数
 int findMax(double a,double b,double c)
 {
     if(a >= b && a >= c) return 1;
     if(b >= a && b >= c) return 2;
     return 3;
 }
-
-// 找最小
+// 找最小函数
 int findMin(double a,double b,double c)
 {
     if(a <= b && a <= c) return 1;
     if(b <= a && b <= c) return 2;
     return 3;
 }
-
-// 输出结果
-void printResult(double a,double b,double c)
+// 输出结果函数
+void Result(double a,double b,double c)
 {
     int max_id = findMax(a,b,c);  // 函数套函数
-    int min_id = findMin(a,b,c);
+    int min_id = findMin(a,b,c);  // 函数套函数x2
 
     double arr[3] = {a,b,c};
-
-    cout << "第" << max_id << "只小猪最重，为" << arr[max_id-1] << "kg" << endl;
-    cout << "第" << min_id << "只小猪最轻，为" << arr[min_id-1] << "kg" << endl;
+    cout << "第" << max_id << "只小猪最重，为" << arr[max_id-1] << "kg" << endl;//输出最大
+    cout << "第" << min_id << "只小猪最轻，为" << arr[min_id-1] << "kg" << endl;//输出最小
 }
-
+//懒人函数
 void input(int i)
 {
     cout<<"第"<<i<<"只"<<endl;
 }
-
+//主函数
 int main()
 {
     double a,b,c;
 
     cout<<"请依次输入小猪的质量"<<endl;
-
     input(1); cin>>a;
     input(2); cin>>b;
     input(3); cin>>c;
-
-    printResult(a,b,c);
+    Result(a,b,c);
 
     return 0;
 }
