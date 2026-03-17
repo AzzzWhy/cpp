78c62b9031bae159860b28f8e23cf1d579e8c9cb
azzwhy <2035698109@qq.com>
2026-03-17 14:07:15 +0800

    calculs

---
+++ ++--.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a=1;
+cout<<a++<<endl;
+cout<<a<<endl;
+//想要运算结果为小数，使用double
+double d1=1.0;
+double d2=3.0;
+cout<<d1/d2<<endl;
+return 0;
+
+
+
+}
