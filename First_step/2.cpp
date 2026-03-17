a3091d79be860e790f6d6a7a1b6fd4a9f6283299
azzwhy <2035698109@qq.com>
2026-03-17 12:25:19 +0800

    tset

---
+++ First_step/2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 1111111;
+    cout<<"a"<<sizeof(a)<<endl;
+    char ch ='a';
+    cout<<"ch"<<ch<<endl;
+
+    return 0;
+
+
+
+}
