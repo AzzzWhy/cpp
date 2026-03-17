fb27fc0f878205b4c9b03d28a5641ce407180aa4
azzwhy <2035698109@qq.com>
2026-03-17 12:53:11 +0800

    bool

---
+++ First_step/3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //转义字符
+    //换行符\n
+    cout<<"hello world\n";
+    cout<<"\\";
+    return 0;
+}
---
+++ bool.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool flag = true;
+    cout<<flag<<endl;
+    flag=false;
+    cout <<flag<<endl;
+    cout<<sizeof (bool)<<endl;
+    return 0;
+
+}
