bc9d5d4396497e887d4748a874ddd5bb5b38413e
AzzzWhy <2035698109@qq.com>
2026-04-20 15:06:19 +0800

    3

--- First_step/array_1.cpp
+++ First_step/array_1.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main()
 {
+    /*
     // 数组
     int arr[5];
     // 给数组元素赋值
@@ -42,4 +43,46 @@ int main()
         cout << arr3[i] << endl;
     }
     return 0;
+
+    ---------------------------------------------------------------------
+
+    // The funtion of the name of one dimensional array
+    // 可以统计整个数组在内存中的长度
+    // 可以获取数组在内存中的首地址
+    // 数组名的用途
+    // 1，可以通过数组名统计整个数组占用内存的大小
+
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    cout << "the space of the array" << "\n"
+         << sizeof(arr) << endl;
+    cout << "The space of per section" << "\n"
+         << sizeof(arr[1]) << endl;
+    cout << "how many sections in this array?" << "\n"
+         << (sizeof(arr) / sizeof(arr[0])) << endl;
+
+    // 2，可以通过数组名查看数组的首地址
+
+    cout << "the address of the array" << "\n"
+         << (long long)arr << endl;
+    cout << "the address of the fiest section of the array" << "\n"
+         << (long long)&arr[0] << endl;
+    cout << "The address of the second section of the arrat" << "\n"
+         << (long long)&arr[1] << endl;
+
+    //数组名是常量，不能进行赋值操作。
+    //arr = 100; not allowed
+
+    -----------------------------------------------------------------------
+    // exercise of the array
+    int max;
+    int arr[5] = {300, 350, 200, 400, 250};
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    cout << "the max is " << max << endl;
+    return 0;*/
 }
